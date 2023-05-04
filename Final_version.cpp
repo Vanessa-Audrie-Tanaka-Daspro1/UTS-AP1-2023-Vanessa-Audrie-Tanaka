c0426b95c2ef5f6685cf2538a6e96aa4469240a4
unknown <vanessaaudrie43@gmail.com>
2023-05-04 19:59:32 +0700

    nengok yang ini aja bang, gatau cara edit file yang udh diupload soalnya

---
+++ Final_version.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+void process(int num) 
+{
+int i = 0;
+
+// upper half of the pattern
+ for (int b=num-1; b>0; b--)
+ {
+    for (int c=b; c>0; c--) // upper left triangle
+    {
+      cout << "* ";
+    }
+
+    for (int c=0; (i*2)>c; c++) // upper middle blank part
+    { 
+      cout << "  ";
+    }
+
+    for (int c=b; c>0; c--) // upper right triangle
+    {
+      cout << "* ";
+    }
+    i++;
+    cout << endl;
+ }
+ 
+// bottom half of the pattern
+i = num-1;
+  for (int b=1; b<num; b++)
+  {
+    for (int c=0; c<b ; c++) // bottom left triangle
+    {
+      cout << "* ";
+    }
+
+    for (int c=0; c<num*2-b*2-2; c++) // bottom middle blank part
+    {
+      cout << "  ";
+    }
+
+    for (int c=0; c<b; c++) // bottom right triangle
+    {
+      cout << "* ";
+    }
+    i--;
+    cout << endl;
+    }
+}
+
+int main() 
+{
+  int num;
+  cout << "Input number : ";
+  cin >> num;
+  process(num);
+
+}
